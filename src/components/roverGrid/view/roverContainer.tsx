83dca3250e76bcc8fea51ce0256238f09b69a0cc
rrmarriott <rich@marriott.me.uk>
2017-03-24 01:14:42 +0800

    Fix some linting issues

--- src/components/roverGrid/view/roverContainer.tsx
+++ src/components/roverGrid/view/roverContainer.tsx
@@ -24,22 +24,25 @@ export default class RoverContainer extends React.Component<any, any> {
                     <button onClick={() => this.executeCommand(Command.TURN_LEFT)}>Left</button>
                     <button onClick={() => this.executeCommand(Command.TURN_RIGHT)}>Right</button>
                     <h4>
-                        Currently Facing: {RoverDirection[rover.direction]}. Position: X = {rover.xPos}, Y: {rover.yPos}</h4>
-                    <h2></h2>
+                        Currently Facing: {RoverDirection[rover.direction]}.
+                        Position: X = {rover.xPos}, Y = {rover.yPos}
+                    </h4>
                     <RoverGrid rover={rover} grid={grid}/>
                 </div>
                 <div>
                     <ul>
                         <li>
-                            You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
+                            You are given the initial starting point (x,y) of a rover and the direction
+                            (N,S,E,W) it is facing.
                         </li>
                         <li>The rover receives a character array of commands.</li>
                         <li>Implement commands that move the rover forward/backward (f,b).</li>
                         <li>Implement commands that turn the rover left/right (l,r).</li>
                         <li>Implement wrapping from one edge of the grid to another. (planets are spheres after all)
                         </li>
-                        <li>Implement obstacle detection before each move to a new square.
-If a given sequence of commands encounters an obstacle, the rover moves up to the last possible point and reports the obstacle.
+                        <li>Implement obstacle detection before each move to a new square. If a given sequence of
+                        commands encounters an obstacle, the rover moves up to the last possible point and reports the
+                        obstacle.
                         </li>
                     </ul>
                 </div>
